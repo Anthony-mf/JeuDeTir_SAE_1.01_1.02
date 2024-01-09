540c9f642caa01297a61cb320b2c867bb5fb4852
anthony-mf <Anthony.Marques-Felix@etu.univ-smb.fr>
2024-01-09 11:35:06 +0100

    Game Engine

--- JeuDeTir_SAE_1.01_1.02/MainWindow.xaml.cs
+++ JeuDeTir_SAE_1.01_1.02/MainWindow.xaml.cs
@@ -24,14 +24,26 @@ namespace JeuDeTir_SAE_1._01_1._02
     {
         private bool allerGauche, allerDroite = false;
         private bool allerHaut, allerBas = false;
-        private int playerSpeed = 7;
-
+        private int vitesseJoueur = 25;
+        private Rect joueur;
+        private DispatcherTimer minuterie = new DispatcherTimer();
 
         private int totalJoueur;
         public MainWindow()
         {
             InitializeComponent();
-            CreationJoueur(1);
+            /*ImageBrush fondWPF = new ImageBrush();
+            fondWPF.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "img/statique/FondCanvas.jpg"));
+            monCanvas.Background = fondWPF;
+            ImageBrush fondMenu = new ImageBrush();
+            fondMenu.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "img/statique/FondMenu.png"));
+            //MenuCanvas.Background = fondMenu;*/
+            Menu menu = new Menu();
+            menu.ShowDialog();
+            minuterie.Tick += GameEngine;
+            // rafraissement toutes les 16 milliseconds
+            minuterie.Interval = TimeSpan.FromMilliseconds(16);
+            minuterie.Start();
         }
 
         private void CreationJoueur(int nbJoueur)
@@ -41,13 +53,13 @@ namespace JeuDeTir_SAE_1._01_1._02
             for (int i = 0; i < nbJoueur; i++)
             {
                 ImageBrush joueurSkin = new ImageBrush();
-                joueurSkin.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "img/Statique/tortue_statique.png"));
+                //joueurSkin.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "images/Statique/tortue_statique.png"));
                 Rectangle nouveauJoueur = new Rectangle
                 {
                     Tag = "joueur",
                     Height = 150,
                     Width = 150,
-                    Fill = joueurSkin,
+                    Fill = Brushes.Red,
                 };
                 Canvas.SetTop(nouveauJoueur, 350);
                 Canvas.SetLeft(nouveauJoueur, gauche);
@@ -56,29 +68,34 @@ namespace JeuDeTir_SAE_1._01_1._02
 
             }
         }
-
-        private void MouvementJoueur()
+        private void GameEngine(object sender, EventArgs e)
         {
-            if (allerGauche && Canvas.GetLeft(joueur) > 0)
+            CreationJoueur(1);
+            foreach (Rectangle x in monCanvas.Children.OfType<Rectangle>())
             {
-                Canvas.SetLeft(joueur, Canvas.GetLeft(joueur) - playerSpeed);
+                MouvementJoueur(x, joueur);
             }
-            else if (allerDroite && Canvas.GetLeft(player) + player1.Width < Application.Current.MainWindow.Width)
-            {
-                Canvas.SetLeft(player, Canvas.GetLeft(player) + playerSpeed);
-            }
-            else if (allerHaut && Canvas.GetTop(joueur) > 0)
-            {
-                Canvas.SetTop(joueur, Canvas.GetTop(joueur) - playerSpeed);
-            }
-            else if (allerBas && Canvas.GetTop(player) + player1.Height < Application.Current.MainWindow.Height)
-            {
-                Canvas.SetTop(player, Canvas.GetTop(player) + playerSpeed);
-            }
-
-
-
         }
+        private void MouvementJoueur(Rectangle x, Rect joueur)
+        {
+            if (x is Rectangle && (string)x.Tag == "joueur" && allerGauche && Canvas.GetLeft(x) > 0)
+            {
+                Canvas.SetLeft(x, Canvas.GetLeft(x) - vitesseJoueur);
+            }
+            else if (x is Rectangle && (string)x.Tag == "joueur" && allerDroite && Canvas.GetLeft(x) + joueur.Width < Application.Current.MainWindow.Width)
+            {
+                Canvas.SetLeft(x, Canvas.GetLeft(x) + vitesseJoueur);
+            }
+            /*if (x is Rectangle && (string)x.Tag == "joueur" && allerHaut && Canvas.GetTop(x) > 0)
+            {
+                Canvas.SetTop(x, Canvas.GetTop(x) - vitesseJoueur);
+            }
+            else if (x is Rectangle && (string)x.Tag == "joueur" && allerBas && Canvas.GetTop(x) + joueur.Height < Application.Current.MainWindow.Height)
+            {
+                Canvas.SetTop(x, Canvas.GetTop(x) + vitesseJoueur);
+            }*/
+        }
+
         private void CanvasToucheBaissée(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Left)
@@ -96,6 +113,25 @@ namespace JeuDeTir_SAE_1._01_1._02
             if (e.Key == Key.Down)
             {
                 allerBas = true;
+            }
+        }
+        private void CanvasToucheRelachée(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                allerGauche = false;
+            }
+            if (e.Key == Key.Right)
+            {
+                allerDroite = false;
+            }
+            if (e.Key == Key.Up)
+            {
+                allerHaut = false;
+            }
+            if (e.Key == Key.Down)
+            {
+                allerBas = false;
             }
         }
 
